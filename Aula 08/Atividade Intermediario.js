06a4f089dd34b20b13ab83d9ee7fecdc86f722bc
Bruno Souza <brunherique09@gmail.com>
2025-10-25 09:06:27 -0300

    Atividade Inter ( Aula 08 )

---
+++ Aula 08/Atividade Intermediario.js
@@ -0,0 +1,23 @@
+console.log ("Contador usando FOR:");
+for (let i=1; i <= 10; i++){
+    console.log(i);
+}
+
+console.log("\n" + "-" .repeat(20) + "\n");
+
+console.log ("Contador usando While:");
+let j = 1;
+while (j <= 10) { 
+    console.log (j);
+    j++;
+}
+
+console.log("\n" + "-" .repeat(20) + "\n");
+
+
+console.log("Contaodr usando DO...WHILE: ");
+let k = 1;
+do {
+    console.log(k);
+    k++;
+}while (k <= 10);
